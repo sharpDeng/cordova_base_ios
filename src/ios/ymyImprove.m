702b97565353da0950dc180e327c838e709da31b
mac <mac@163.com>
2018-01-02 17:03:29 +0800

    插件初始化

---
+++ src/ios/ymyImprove.m
@@ -0,0 +1,50 @@
+/********* ymyImprove.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+#import "MainViewController.h"
+
+@interface ymyImprove : CDVPlugin <UIGestureRecognizerDelegate> {
+  // Member variables go here.
+}
+
+- (void)pluginInitialize；
+@end
+
+@implementation ymyImprove
+
+- (void)pluginInitialize
+{
+    MainViewController *vc = [self getCurrentMainViewController];
+    vc.webView.scrollView.bounces = NO;
+    UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:nil];
+    longPress.delegate = self;
+    longPress.minimumPressDuration = 0.4;
+    [vc.webView addGestureRecognizer:longPress];
+}
+
+- (MainViewController *)getCurrentMainViewController
+{
+    UIViewController *result = nil;
+    UIWindow *window = [[UIApplication sharedApplication]keyWindow];
+    if(window.windowLevel != UIWindowLevelNormal)
+    {
+        NSArray *windows = [[UIApplication sharedApplication] windows];
+        for(UIWindow *tempWin in windows)
+        {
+            if(tempWin.windowLevel == UIWindowLevelNormal)
+            {
+                window = tempWin;
+                break;
+            }
+        }
+    }
+    UIView *frontView = [[window subviews] objectAtIndex:0];
+    id nextResponder = [frontView nextResponder];
+    //当前的响应者是否属于UIViewController类
+    if([nextResponder isKindOfClass:[UIViewController class]])
+        result = nextResponder;
+    else
+        result = window.rootViewController;
+    return (MainViewController*)result;
+}
+@end
---
+++ www/ymyImprove.js
@@ -0,0 +1,10 @@
+var exec = require('cordova/exec');
+
+exports.coolMethod = function (arg0, success, error) {
+    // exec(success, error, 'ymy-improve', 'coolMethod', [arg0]);
+};
+
+window.addEventListener("load", function(){
+    document.documentElement.style.webkitUserSelect='none';
+    document.documentElement.style.webkitTouchCallout='none';
+}, false);
